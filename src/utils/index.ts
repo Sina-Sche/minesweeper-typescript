132b2bab8138e2cee70173773b6c390a1c386422
Sina-Sche <si.scheckenbach@gmail.com>
2021-01-30 18:09:17 +0100

    Add generate bombs function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,10 @@
-import { MAX_COLS, MAX_ROWS } from "../constants";
+import { MAX_COLS, MAX_ROWS, NO_OF_BOMBS } from "../constants";
 import { Cell, CellState, CellValue } from "../types";
 
 export const generateCells = (): Cell[][] => {
-  const cells: Cell[][] = [];
+  let cells: Cell[][] = [];
 
+  // generating all cells
   for (let row = 0; row < MAX_ROWS; row++) {
     cells.push([]);
     for (let col = 0; col < MAX_COLS; col++) {
@@ -11,6 +12,30 @@ export const generateCells = (): Cell[][] => {
         value: CellValue.none,
         state: CellState.untouched,
       });
+    }
+  }
+
+  // randomly generate 10 or more bombs;
+
+  let bombsPlaced = 0;
+  while (bombsPlaced < NO_OF_BOMBS) {
+    const randomRow = Math.floor(Math.random() * MAX_ROWS);
+    const randomCol = Math.floor(Math.random() * MAX_COLS);
+
+    const currentCell = cells[randomRow][randomCol];
+    if (currentCell.value !== CellValue.bomb) {
+      cells = cells.map((row, rowIndex) =>
+        row.map((cell, colIndex) => {
+          if (randomRow === rowIndex && randomCol === colIndex) {
+            return {
+              ...cell,
+              value: CellValue.bomb,
+            };
+          }
+          return cell;
+        })
+      );
+      bombsPlaced++;
     }
   }
   return cells;
